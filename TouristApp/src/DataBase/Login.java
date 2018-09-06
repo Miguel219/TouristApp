44b82e1c1d42a150da95693505badcb800217e61
André Rodríguez <rod18332@uvg.edu.gt>
2018-09-06 10:58:23 -0600

    Cambios andré

---
+++ TouristApp/src/DataBase/Login.java
@@ -0,0 +1,30 @@
+package DataBase;
+
+import javafx.scene.control.TextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+public class Login {
+	 @FXML
+	    private TextField user;
+
+	    @FXML
+	    private TextField pass;
+
+	    @FXML
+	    private Button login;
+
+	    @FXML
+	    private Button sign;
+
+	    @FXML
+	    void login(ActionEvent event) {
+	    	
+	    }
+
+
+}
---
+++ TouristApp/src/module-info.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+/**
+ * @author andre
+ *
+ */
+module TouristApp {
+	exports administrador;
+	exports application;
+	exports DataBase;
+
+	requires java.desktop;
+	requires java.sql;
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.swing;
+}
