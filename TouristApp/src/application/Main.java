98dfa87a1b1366d4341050f89fd4d42bf535b413
Miguel219 <34550700+Miguel219@users.noreply.github.com>
2018-09-06 19:53:47 -0600

    Cambios

--- TouristApp/src/application/Main.java
+++ TouristApp/src/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -10,8 +11,8 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Administrador.fxml"));;
+			Scene scene = new Scene(root,400,550);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
